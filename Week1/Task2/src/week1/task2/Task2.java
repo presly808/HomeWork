d65ba367aa78dc1178777e91a3c09082a202cafc
Chernyshovdev <Razer@MacBook-Air-John.local>
2015-05-23 20:20:47 +0300

    Добавлено 2 метода

--- Week1/Task2/src/week1/task2/Task2.java
+++ Week1/Task2/src/week1/task2/Task2.java
@@ -8,23 +8,37 @@ public class Task2
     public static void main(String[] args)
     {
         final int SizeOfArray = 3;
-        int max = 0;
         Scanner scanner = new Scanner(System.in);
         int[] ArrayForNumbers = new int[SizeOfArray];
         for (int i = 0; i < ArrayForNumbers.length; i++) {
             System.out.println("Enter number :");
             ArrayForNumbers[i] = scanner.nextInt();
         }
-        int min = ArrayForNumbers[0];
-        for (int i = 0; i < ArrayForNumbers.length; i++) {
-            if (ArrayForNumbers[i] > max) {
-                max = ArrayForNumbers[i];
-            }
-            if (ArrayForNumbers[i] < min) {
-                min = ArrayForNumbers[i];
-            }
-        }
-        System.out.println("Max=" + max + " Min=" + min);
+
+        System.out.println("Max=" + Maximum(ArrayForNumbers) + " Min=" + Minimum(ArrayForNumbers));
     }
 
+    public static int Minimum(int Array[])
+    {
+        int min = Array[0];
+        for (int i = 0; i < Array.length; i++) {
+            if (Array[i] < min) {
+                min = Array[i];
+            }
+
+        }
+        return min;
+    }
+
+    public static int Maximum(int Array[])
+    {
+        int max = Array[0];
+        for (int i = 0; i < Array.length; i++) {
+            if (Array[i] > max) {
+                max = Array[i];
+            }
+        }
+        return max;
+
+    }
 }
