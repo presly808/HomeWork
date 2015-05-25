c904c7d08e3794953892ee19235b66c46e2d3d7e
presly808 <presly808@gmail.com>
2015-05-25 13:09:45 +0300

    Update Task1.java
    read comments

--- Week1/Task1/src/week1/task1/Task1.java
+++ Week1/Task1/src/week1/task1/Task1.java
@@ -2,16 +2,16 @@ package week1.task1;
 
 import java.util.Scanner;
 
-public class Task1
+public class Task1 // see java naming convetion
 {
-
+    // if you use intellij idea, press Ctrl + Alt + L for formatting code
     public static void main(String[] args)
     {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Enter time of call :");
-        try {
-            byte TimeOfCall = scanner.nextByte();
-            if (TimeOfCall < 0 | TimeOfCall > 24) {
+        System.out.println("Enter time of call :"); 
+        try {  // why did you use try-catch, do you know about exceptions
+            byte TimeOfCall = scanner.nextByte(); // variable must has name than begins with lower case 
+            if (TimeOfCall < 0 | TimeOfCall > 24) { // instead | use || 
                 System.out.println("Error not correct time,use only range 0-24");
             }
             else {
